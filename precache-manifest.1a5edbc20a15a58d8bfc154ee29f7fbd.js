f22aab82bdf6b64af8105601add830d2c24d2595
ymintyan <9zhenya4@gmail.com>
2020-05-23 20:21:23 +0300

    Updates

---
+++ precache-manifest.1a5edbc20a15a58d8bfc154ee29f7fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0029d603eef5f65a10113e8009e79029",
+    "url": "/msi/index.html"
+  },
+  {
+    "revision": "93846702d693d81c0054",
+    "url": "/msi/static/css/main.a4fdb579.chunk.css"
+  },
+  {
+    "revision": "3d2ab1c16c6ef3d0dd4b",
+    "url": "/msi/static/js/2.3d18427f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/msi/static/js/2.3d18427f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93846702d693d81c0054",
+    "url": "/msi/static/js/main.82eef968.chunk.js"
+  },
+  {
+    "revision": "62282bb35f3d75bd7d0c",
+    "url": "/msi/static/js/runtime-main.2b994441.js"
+  }
+]);
